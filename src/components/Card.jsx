28601e2fc24e5c434d6c7e200cbbd67e81d483f9
Brian Almada <brianalexisalmada84@gmail.com>
2023-10-09 21:31:28 -0300

    Corregir error de sintaxis en el archivo Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ function Card({title, imagSourse, url}) {
             <div className="card-body text-light">
                 <h1 className="card-title">{title}</h1>
                 <p className="card-text text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti tenetur necessitatibus quasi natus facilis fuga et repellat cupiditate ut dolorum? Odit illum doloremque voluptatum eum distinctio totam harum aliquid! Quis?</p>
-                <a href={url} className='btn btn-online-secondary rounded-0' target="blank">
+                <a href={url} className='btn btn-outline-secondary rounded-0' target="blank">
                 Más de esto aquí
                 </a>
             </div>
