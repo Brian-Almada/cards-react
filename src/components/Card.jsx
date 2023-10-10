a7547f0bcfe4adf5d6c3b4cefedd154f7b0fc5f3
Brian Almada <brianalexisalmada84@gmail.com>
2023-10-09 21:21:15 -0300

    Agregar prop para linkear a otra pag

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 
 
-function Card({title, imagSourse}) {
+function Card({title, imagSourse, url}) {
     return(
         <div className="card text-center bg-dark">
             <img src={imagSourse} alt="" />
             <div className="card-body text-light">
                 <h1 className="card-title">{title}</h1>
                 <p className="card-text text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti tenetur necessitatibus quasi natus facilis fuga et repellat cupiditate ut dolorum? Odit illum doloremque voluptatum eum distinctio totam harum aliquid! Quis?</p>
-                <a href="#!" className='btn btn-online-secondary rounded-0'>
+                <a href={url} className='btn btn-online-secondary rounded-0' target="blank">
                 Más de esto aquí
                 </a>
             </div>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -14,47 +14,56 @@ const cards = [
     {
         id: 1,
         title: 'Tales de Mileto',
-        image: img1
+        image: img1,
+        url:'https://es.wikipedia.org/wiki/Tales_de_Mileto'
     },
     {
         id: 2,
         title: 'Anaxímandro',
-        image: img2
+        image: img2,
+        url:'https://es.wikipedia.org/wiki/Anaximandro'
     },
     {
         id: 3,
         title: 'Anaxímenes',
-        image: img3
+        image: img3,
+        url:'https://es.wikipedia.org/wiki/Anax%C3%ADmenes'
     },
     {
         id: 4,
         title: 'Pitágoras',
-        image: img4
+        image: img4,
+        url:'https://es.wikipedia.org/wiki/Pit%C3%A1goras'
     },
     {
         id: 5,
         title: 'Heráclito',
-        image: img5
+        image: img5,
+        url:'https://es.wikipedia.org/wiki/Her%C3%A1clito'
     },
     {
         id: 6,
         title: 'Parménides',
-        image: img6
+        image: img6,
+        url:'https://es.wikipedia.org/wiki/Parm%C3%A9nides_de_Elea'
     },
     {
         id: 7,
         title: 'Empédocles',
-        image: img7
+        image: img7,
+        url:'https://es.wikipedia.org/wiki/Emp%C3%A9docles'
     },
     {
         id: 8,
         title: 'Anaxágoras',
-        image: img8
+        image: img8,
+        url:'https://es.wikipedia.org/wiki/Anax%C3%A1goras'
     },
     {
         id: 9,
         title: 'Demócrito',
-        image: img9
+        image: img9,
+        url:'https://es.wikipedia.org/wiki/Dem%C3%B3crito'
     }
 ]
 
@@ -63,9 +72,9 @@ function Cards() {
         <div className='conteiner d-flex justify-content-center align-items-center h100'>
             <div className='row'>
                 {
-                    cards.map(card => (
+                    cards.map((card) => (
                         <div className='col-md-4' key={card.id}>
-                            <Card title={card.title} imagSourse={card.image}/>
+                            <Card title={card.title} imagSourse={card.image} url={card.url}/>
                         </div>
                     ))
                 }
